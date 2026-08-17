de8c3e419066fb65ba6f876b3f066a24b4c8b4b0
ivanpupsik56-ship-it <ivanpupsik56@gmail.com>
2026-08-17 12:18:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,75 @@
-let score = 0;
+const tg = window.Telegram.WebApp;
+
+tg.ready();
+tg.expand();
+
+
+let score = Number(localStorage.getItem("clicks")) || 0;
+
 
 const text = document.getElementById("score");
 const btn = document.getElementById("click");
+const avatar = document.querySelector(".avatar");
+
+
+text.innerHTML = score;
+
+
 
 btn.onclick = () => {
 
-score++;
+    score++;
 
-text.innerHTML = score;
+    text.innerHTML = score;
+
+
+    // сохраняем
+    localStorage.setItem(
+        "clicks",
+        score
+    );
+
+
+    // вибрация в Telegram
+    if(tg.HapticFeedback){
+
+        tg.HapticFeedback.impactOccurred(
+            "medium"
+        );
+
+    }
+
+
+    // анимация лица
+    avatar.classList.add("jump");
+
+
+    setTimeout(()=>{
+
+        avatar.classList.remove("jump");
+
+    },150);
+
+
+
+    // +1 эффект
+    let plus = document.createElement("div");
+
+    plus.innerHTML="+1";
+
+    plus.className="plus";
+
+
+    document.querySelector(".game")
+    .appendChild(plus);
+
+
+
+    setTimeout(()=>{
+
+        plus.remove();
+
+    },800);
+
 
 };
